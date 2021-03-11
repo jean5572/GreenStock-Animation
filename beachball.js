e8b9cedc9cc7bf0be13104d1c34eeadd40d3ad5e
Jean Wong <jean5572@stud.kea.dk>
2021-03-11 11:01:52 +0100

    added timeline

--- beachball.js
+++ beachball.js
@@ -1,12 +1,28 @@
 "use strict";
-gsap.to(".ball", { duration: 2 });
-gsap.to(".ball", {
-  duration: 2,
-  x: 400,
-  rotation: 200,
-  onComplete: done,
-});
+const timeline = gsap.timeline();
 
-function done() {
-  gsap.to(".ball", { duration: 0.5, scaleY: 0.1, y: 40 });
-}
+timeline
+  .to(".ball", {
+    duration: 2,
+    x: 400,
+    rotation: 200,
+  })
+  .to(".ball", {
+    duration: 0.5,
+    scaleY: 0.1,
+    y: 40,
+  })
+  .to(".ball", {
+    x: 1500,
+    y: -1000,
+  });
+// gsap.to(".ball", {
+//   duration: 2,
+//   x: 400,
+//   rotation: 200,
+//   onComplete: done,
+// });
+
+// function done() {
+//   gsap.to(".ball", { duration: 0.5, scaleY: 0.1, y: 40 });
+// }
